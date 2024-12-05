c7e231900ea08778da9472ca3df7e59a5093d8fa
Daryn Lam <dkl67@drexel.edu>
2024-12-05 12:01:04 -0500

    Update help

--- help.php
+++ help.php
@@ -11,12 +11,12 @@
     <header class="navigation">
         <div class="links">
             <a href="index.php"><i class="fa fa-cutlery logo"></i></a>
-            <a class="help-back" href="index.php"><i class="fa fa-chevron-left back"></i>BACK</a>
+            <a class="help-back" href="recipes.php"><i class="fa fa-chevron-left back"></i>RECIPES</a>
         </div>
     </header>
     <section class="help-block">
         <h3>WHAT DO I DO?</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Use the search bar on the <a href="recipes.php">RECIPES</a> page to find recipes by name, cuisine, or ingredients.</p>
     </section>
 </body>
 </html>
