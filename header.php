139894f2d0425e738ef2e80169c534f24e7ddeb0
Daryn Lam <dkl67@drexel.edu>
2024-12-12 20:53:36 -0500

    Update header

--- header.php
+++ header.php
@@ -1,8 +1,6 @@
 <header class="navigation">
-    <div class="links">
-        <a href="index.php"><i class="fa fa-cutlery logo"></i></a>
-        <a href="index.php">Home</a>
-        <a href="recipes.php">Recipes</a>
-        <a href="help.php">Help</a>
-    </div>
+    <a href="index.php"><i class="fa fa-cutlery logo"></i></a>
+    <a href="index.php">Home</a>
+    <a href="recipes.php">Recipes</a>
+    <a href="help.php">Help</a>
 </header>
