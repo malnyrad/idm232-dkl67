ef97967dcd385283baed31233eac255c49226b2f
Daryn Lam <dkl67@drexel.edu>
2024-12-12 19:21:04 -0500

    Add error checks

--- recipe.php
+++ recipe.php
@@ -1,7 +1,7 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
 
     // connect to database
     require_once 'database_connection.php';
