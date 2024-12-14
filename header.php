83584da5c45111a2da49601f43798746ab8fc218
Daryn Lam <dkl67@drexel.edu>
2024-12-14 17:55:38 -0500

    Animate header, update home page

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header class="navigation">
     <a href="index.php"><i class="fa fa-cutlery logo"></i></a>
-    <a href="index.php">Home</a>
-    <a href="recipes.php">Recipes</a>
-    <a href="help.php">Help</a>
+    <a href="index.php" class="navigation-link left-to-right">Home</a>
+    <a href="recipes.php" class="navigation-link left-to-right">Recipes</a>
+    <a href="help.php" class="navigation-link left-to-right">Help</a>
 </header>
